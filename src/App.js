50737310c136c57e9cf229a8f1b5ad4a66b1687e
Arvin mostafaei <Arvinmostafaeii@gmail.com>
2022-03-05 17:18:09 +0330

    deleted app.css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import logo from پچ لباسlogo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
