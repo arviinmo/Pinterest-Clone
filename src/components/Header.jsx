b8f335277516e680ab8136a6fb8ecf4fc811f412
Arvin mostafaei <Arvinmostafaeii@gmail.com>
2022-03-07 08:19:19 +0330

    adding some style to SearchBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,10 +17,18 @@ function Header() {
       <FollowingButton>
         <a href="/">Following</a>
       </FollowingButton>
-      {/* <SearchWrapper>
-        <SearchBarWrapper></SearchBarWrapper>
+      <SearchWrapper>
+        <SearchBarWrapper>
+          {/* <IconButton>
+            <SearchIcon />
+          </IconButton> */}
+          <form>
+            <input type="text" />
+            <button type="submit"></button>
+          </form>
+        </SearchBarWrapper>
       </SearchWrapper>
-      <IconsWrapper></IconsWrapper> */}
+      {/* <IconsWrapper></IconsWrapper> */}
     </Wrapper>
   );
 }
@@ -44,7 +52,7 @@ const LogoWrapper = styled.div`
   }
 `;
 
-const HomePageButton = styled.div`
+const HomeButtons = styled.div`
   display: flex;
   height: 48px;
   min-width: 123px;
@@ -52,28 +60,41 @@ const HomePageButton = styled.div`
   justify-content: center;
   border-radius: 24px;
   cursor: pointer;
+`;
+
+const HomePageButton = styled(HomeButtons)`
   background-color: rgb(17, 17, 17);
   
-
   a {
     text-decoration: none;
     color: white;
     font-weight: 700;
   }
-`
+`;
 
-const FollowingButton = styled.div`
-  display: flex;
-  height: 48px;
-  min-width: 123px;
-  align-items: center;
-  justify-content: center;
-  border-radius: 24px;
-  cursor: pointer;
+const FollowingButton = styled(HomeButtons)`
   background-color: white;
 
   a {
     color: black;
     font-weight: 700;
   }
-`
+
+  :hover {
+    background-color: #e1e1e1;
+  }
+`;
+
+const SearchWrapper = styled.div`
+  flex: 1;
+`;
+
+const SearchBarWrapper = styled.div`
+  background-color: #efefef;
+  display: flex;
+  height: 48px;
+  width: 100%;
+  border-radius: 50px;
+  border: none;
+  padding-left: 10px;
+`;
